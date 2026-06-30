f9a536c73336f71f39e41b963638d21083ad61af
unknown <arizonarpukraine@gmail.com>
2026-06-30 19:07:02 +0300

    Імпортовано функцію та додано обчислення площі зі сторонами 8 і 5

---
+++ main.py
@@ -0,0 +1,6 @@
+from math_functions import calculate_rectangle_area
+if __name__ == "__main__":
+    width = 8
+    height = 5
+    area = calculate_rectangle_area(width, height)
+    print(f"Площа прямокутника зі сторонами {width} і {height} дорівнює: {area}")
