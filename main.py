895501256f6094b3aa25d0611b97123290a4b32a
unknown <arizonarpukraine@gmail.com>
2026-06-30 19:46:23 +0300

    Додано виклик функції rectangle_perimeter у main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from math_functions import calculate_rectangle_area
+from math_functions import calculate_rectangle_area, rectangle_perimeter
 if __name__ == "__main__":
     width = 8
     height = 5
     area = calculate_rectangle_area(width, height)
     print(f"Площа прямокутника зі сторонами {width} і {height} дорівнює: {area}")
+    perimeter = rectangle_perimeter(width, height)
+    print(f"Периметр прямокутника зі сторонами {width} і {height} дорівнює: {perimeter}")
