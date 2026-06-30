2ea2b66007695ffa76564207a1cfc361fd34c8f9
unknown <arizonarpukraine@gmail.com>
2026-06-30 19:45:56 +0300

    Створено функцію rectangle_perimeter

--- math_functions.py
+++ math_functions.py
@@ -1,3 +1,5 @@
 def calculate_rectangle_area(width, height):
-    """Обчислює площу прямокутника за його сторонами."""
     return width * height
+
+def rectangle_perimeter(width, height):
+    return 2 * (width + height)
